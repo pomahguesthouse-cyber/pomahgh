4c8757ba420901972553215baebbb1f6062b0a6d
pomahguesthouse-cyber <pomahguesthouse@gmail.com>
2026-03-18 12:11:01 +0700

    optimasi

--- src/App.tsx
+++ src/App.tsx
@@ -109,8 +109,6 @@ const App = () => (
           <Route path="/admin/booking-com" element={<AdminLayout><AdminBookingCom /></AdminLayout>} />
           <Route path="/admin/bank-accounts" element={<AdminLayout><AdminBankAccounts /></AdminLayout>} />
           <Route path="/admin/seo-settings" element={<AdminLayout><AdminSeoSettings /></AdminLayout>} />
-          <Route path="/admin/pages" element={<AdminLayout><AdminPages /></AdminLayout>} />
-          <Route path="/admin/landing-pages" element={<AdminLayout><AdminPages /></AdminLayout>} />
           <Route path="/admin/media-library" element={<AdminLayout><AdminMediaLibrary /></AdminLayout>} />
           
           <Route path="/admin/city-attractions" element={<AdminLayout><AdminCityAttractions /></AdminLayout>} />
